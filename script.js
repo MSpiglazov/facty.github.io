00b2c308f3d982d7f0788bb555e0638e80872611
MSpiglazov <m.spiglazov@yandex.ru>
2025-01-09 17:21:15 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const burger = document.getElementById('burger')
-const nav = document.getElementById('nav')
-
-burger.addEventListener('click', function(){
-  burger.classList.toggle('active')
-  nav.classList.toggle('active-nav__block')
-  
-})
